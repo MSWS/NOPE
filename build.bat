40a3b4cb93c2c5f163625fa3a9e75d97081eaca3
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-20 18:04:02 +1200

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-title NOPE Build
-call mvn clean install
-@copy C:\Users\imodm\eclipse-workspace\NOPE\target\NOPE.jar C:\Users\imodm\Documents\Servers\TestServer\plugins /y 
-echo Successfully built and copied
-timeout 5
