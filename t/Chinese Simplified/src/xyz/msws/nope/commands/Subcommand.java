c783cd6cbc7559fea8ea17220cbaeff15fa5021e
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:51:39 +1200

    New translations Subcommand.java (Chinese Simplified)

--- t/Chinese Simplified/src/xyz/msws/nope/commands/Subcommand.java
+++ t/Chinese Simplified/src/xyz/msws/nope/commands/Subcommand.java
@@ -1,46 +1,46 @@
-package xyz.msws.nope.commands;
+包 xyz.msws.nope.commands;
 
-import java.util.ArrayList;
-import java.util.List;
+导入 java.util.ArrayList;
+导入 java.util.List;
 
-import javax.annotation.Nullable;
+导入 javax.annotation.Nullab;
 
 import org.bukkit.command.CommandSender;
 
-import xyz.msws.nope.NOPE;
+导入 xyz.msws.nope.NOPE;
 
 /**
- * Represents a subcommand of a main command. It is expected each subcommand
- * handles its logic internally. This can include having sub commands of sub
- * commands.
+ * 代表一个主命令的子命令。 预计每个子命令
+ * 从内部处理逻辑。 这可以包括子命令的子命令
+ * 命令。
  * 
  * @author imodm
  *
  */
-public abstract class Subcommand {
+公共抽象类子命令
 
-	protected NOPE plugin;
+	受保护的 NOPE 插件；
 
-	public Subcommand(NOPE plugin) {
-		this.plugin = plugin;
+	公开Subcommand(NOPE plugin)
+		插件 = 插件；
 	}
 
 	@Nullable
-	public abstract List<String[]> tabCompletions(CommandSender sender);
+	公开摘要列表<String[]> tabCompltions(Commander Sender)；
 
-	public abstract String getName();
+	公开摘要字符串GetName();
 
-	public abstract String getUsage();
+	公共摘要字符串getUsage();
 
-	public abstract String getDescription();
+	公开摘要字符串getDescription();
 
-	public List<String> getAliases() {
-		return new ArrayList<>();
+	公开列表<String> getAliases()
+		返回新的数组列表<>();
 	}
 
-	public String getPermission() {
-		return null;
+	public String getPermission()
+		返回 null;
 	}
 
-	public abstract CommandResult execute(CommandSender sender, String[] args);
+	公共抽象命令执行(Commander Sender, String[]args)；
 }
