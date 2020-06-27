51d1bf95daf4c30fe267cd58181bab37b2c82745
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:48:04 +1200

    New translations NOPECommand.java (Norwegian)

--- t/Norwegian/src/xyz/msws/nope/commands/NOPECommand.java
+++ t/Norwegian/src/xyz/msws/nope/commands/NOPECommand.java
@@ -1,73 +1,73 @@
-package xyz.msws.nope.commands;
+pakke xyz.msws.nope.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import xyz.msws.nope.NOPE;
-import xyz.msws.nope.commands.sub.BanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ChecksSubcommand;
-import xyz.msws.nope.commands.sub.ClearSubcommand;
-import xyz.msws.nope.commands.sub.EnablechecksSubcommand;
-import xyz.msws.nope.commands.sub.HelpSubcommand;
-import xyz.msws.nope.commands.sub.LookupSubcommand;
-import xyz.msws.nope.commands.sub.OnlineSubcommand;
-import xyz.msws.nope.commands.sub.ReloadSubcommand;
-import xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ReportSubcommand;
-import xyz.msws.nope.commands.sub.ResetSubcommand;
-import xyz.msws.nope.commands.sub.StatsSubcommand;
-import xyz.msws.nope.commands.sub.TestAnimationSubcommand;
-import xyz.msws.nope.commands.sub.TestlagSubcommand;
-import xyz.msws.nope.commands.sub.TimeSubcommand;
-import xyz.msws.nope.commands.sub.ToggleSubcommand;
-import xyz.msws.nope.commands.sub.TrustSubcommand;
-import xyz.msws.nope.commands.sub.VLSubcommand;
-import xyz.msws.nope.commands.sub.WarnSubcommand;
+importer xyz.msws.nope.NOPE;
+importer xyz.msws.nope.commands.sub.BanwaveSubcommand;
+Importer xyz.msws.nope.commands.sub.ChecksSubcommand;
+Importer xyz.msws.nope.commands.sub.Clearsubcommand;
+Importer xyz.msws.nope.commands.sub.EnablechecksSubcommand;
+Importer xyz.msws.nope.commands.sub.HelpSubcommand;
+importer xyz.msws.nope.commands.sub.Lookupsubcommand;
+Importer xyz.msws.nope.commands.sub.OnlineSubcommand;
+importer xyz.msws.nope.commands.sub.Reloadsubkommando;
+importer xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
+importer xyz.msws.nope.commands.sub.ReportSubcommand;
+Importer xyz.msws.nope.commands.sub.ResetSubcommand;
+importere xyz.msws.nope.commands.sub.StatsSubcommand;
+importer xyz.msws.nope.commands.sub.TestAnimationUnderkommando;
+importer xyz.msws.nope.commands.sub.Testlagsubkommando;
+importer xyz.msws.nope.commands.sub.TimeSubcommand;
+Importer xyz.msws.nope.commands.sub.ToggleSubcommand;
+importer xyz.msws.nope.commands.sub.Trustsubkommando;
+Importer xyz.msws.nope.commands.sub.VLSubcommand;
+Importer xyz.msws.nope.commands.sub.Warnsubkommando;
 
-public class NOPECommand extends AbstractCommand {
+offentlig klasse NOPECommand utvider AbstractCommand {
 
-	private HelpSubcommand help;
+	privat hjelp hjelp av underkommandoen;
 
-	public NOPECommand(NOPE plugin) {
+	offentlig NOPECommand(NOPE plugin) {
 		super(plugin);
-		help = new HelpSubcommand(plugin, this, 8);
+		hjelp = ny HelpSubcommand(plugin, dette, 8);
 	}
 
-	@Override
+	@Overstyring
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		label = label.toUpperCase();
-		if (super.onCommand(sender, command, label, args))
-			return true;
-		help.execute(sender, args);
-		return true;
+		etikett = label.toUpperCase();
+		hvis (super.onCommand(sender, kommando, args))
+			retursann;
+		"help.execute(sender, args);
+		retursann;
 	}
 
-	@Override
-	public void enable() {
-		super.enable();
-		cmds.add(new VLSubcommand(plugin));
-		cmds.add(new ClearSubcommand(plugin));
-		cmds.add(new ReportSubcommand(plugin));
-		cmds.add(new LookupSubcommand(plugin));
-		cmds.add(new ToggleSubcommand(plugin));
-		cmds.add(new StatsSubcommand(plugin));
-		cmds.add(new TimeSubcommand(plugin));
-		cmds.add(new BanwaveSubcommand(plugin));
-		cmds.add(new ReloadSubcommand(plugin));
-		cmds.add(new ResetSubcommand(plugin));
-		cmds.add(new TestlagSubcommand(plugin));
-		cmds.add(new RemovebanwaveSubcommand(plugin));
-		cmds.add(new EnablechecksSubcommand(plugin));
-		cmds.add(new OnlineSubcommand(plugin));
-		cmds.add(new TestAnimationSubcommand(plugin));
-		cmds.add(new WarnSubcommand(plugin));
-		cmds.add(new ChecksSubcommand(plugin));
-		cmds.add(new TrustSubcommand(plugin));
-		cmds.add(help);
+	@Overstyring
+	offentlig aktivering() {
+		over.aktiver();
+		cmds.add(ny VLSubcommand(plugin));
+		cmds.add(ny ClearSubcommand(plugin));
+		cmds.add(ny ReportUndercommand(plugin));
+		cmds.add(ny LookupSubcommand(plugin));
+		cmds.add(ny ToggleSubcommand(plugin));
+		cmds.add(ny StatsSubcommand(plugin));
+		cmds.add(ny TimeSubcommand(plugin));
+		cmds.add(ny BanwaveSubcommand(plugin));
+		cmds.add(ny ReloadSubcommand(plugin));
+		cmds.add(ny ResetSubcommand(plugin));
+		cmds.add(ny TestlagSubcommand(plugin));
+		cmds.add(ny RemovebanwaveSubcommand(plugin));
+		cmds.add(ny EnablechecksSubcommand(plugin));
+		cmds.add(ny OnlineSubcommand(plugin));
+		cmds.add(ny TestAnimationSubcommand(plugin));
+		cmds.add(ny WarnSubcommand(plugin));
+		cmds.add(ny ChecksSubcommand(plugin));
+		cmds.add(ny TrustSubcommand(plugin));
+		cmds.add(hjelp);
 	}
 
-	@Override
+	@Overstyring
 	public String getName() {
-		return "nope";
+		returner "uavn";
 	}
 }
