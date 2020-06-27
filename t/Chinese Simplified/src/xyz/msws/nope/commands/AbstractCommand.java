0f035d6a485a48932ca8060ffe7161c4d27907ee
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:51:43 +1200

    New translations AbstractCommand.java (Chinese Simplified)

--- t/Chinese Simplified/src/xyz/msws/nope/commands/AbstractCommand.java
+++ t/Chinese Simplified/src/xyz/msws/nope/commands/AbstractCommand.java
@@ -1,7 +1,7 @@
-package xyz.msws.nope.commands;
+包 xyz.msws.nope.commands;
 
-import java.util.ArrayList;
-import java.util.List;
+导入 java.util.ArrayList;
+导入 java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,100 +9,100 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
 
-import xyz.msws.nope.NOPE;
-import xyz.msws.nope.modules.AbstractModule;
-import xyz.msws.nope.utils.MSG;
+导入 xyz.msws.nope.NOPE;
+导入 xyz.msws.nope.modules.AbstractModule;
+导入 xyz.msws.nope.utils.MSG;
 
-public abstract class AbstractCommand extends AbstractModule implements CommandExecutor, TabCompleter {
+公共抽象类的摘要命令扩展摘要模块实现指挥官，TabCompleter v.
 
-	protected List<Subcommand> cmds = new ArrayList<>();
+	受保护的列表<Subcommand> cmds = 新的数组列表<>();
 
-	public AbstractCommand(NOPE plugin) {
-		super(plugin);
+	公开的 AbstractCommand(NOPE plugin)
+		super(插件)；
 	}
 
-	public abstract String getName();
+	公开摘要字符串GetName();
 
-	@Override
-	public void enable() {
-		PluginCommand cmd = plugin.getCommand(getName());
-		cmd.setExecutor(this);
-		cmd.setTabCompleter(this);
+	@覆盖
+	公开无效启用()
+		PluginCommand cmd = plugin.getCommand(getName() );
+		cmd.setExecutor(这是);
+		cmd.setTabCompleter (this);
 	}
 
-	public void disable() {
-		PluginCommand cmd = plugin.getCommand(getName());
+	public invoice disabl()
+		PluginCommand cmd = plugin.getCommand(getName() );
 		cmd.setExecutor(null);
-		cmd.setTabCompleter(null);
+		cmd.setTabComplter(null)；
 	}
 
-	public List<Subcommand> getSubCommands() {
-		return cmds;
+	公开列表<Subcommand> getSubCommands()
+		返回cmds；
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	@覆盖
+	Public boolian onCommander(Commander Sender, Command Command, String label, String[]args)
 		if (args.length < 1)
-			return false;
-		String cmd = args[0];
-		for (Subcommand c : cmds) {
-			if (c.getName().equalsIgnoreCase(cmd) || c.getAliases().contains(cmd.toLowerCase())) {
-				if (c.getPermission() != null && !sender.hasPermission(c.getPermission())) {
-					MSG.tell(sender,
+			返回 false;
+		字符串 cmd = args[0];
+		for (Subcommand c : cmds)
+			if (c.getName().equalsIgnoreCase(cmd) || c.getAliases().contains(cmd.toLowerCase()))
+				if (c.getPermission) != null && !sender.hasPermission(c.getPermission()))
+					MSG.tell(发件人)
 							MSG.getString("Command.NoPermission",
-									"&4&l[&c&lNOPE&4&l] &cYou lack the &a%perm% &cpermission.")
-									.replace("%perm%", c.getPermission()));
-					return true;
+									"&4&l[&c&lNOPE&4&l] &c你缺少&a%perm% &cpermission。")
+									.replace("%perm%", c.getPermission());
+					返回真；
 				}
-				CommandResult result = c.execute(sender, args);
-				if (result == CommandResult.SUCCESS)
-					return true;
-				if (result == CommandResult.NO_PERMISSION) {
-					MSG.tell(sender,
+				CommandResults = c.execute(发送者、args)；
+				if (results == CommandResult.SUCCESS)
+					返回真；
+				if (results == CommandResult.NO_PERMISSION)
+					MSG.tell(发件人)
 							MSG.getString("Command.NoPermission",
-									"&4&l[&c&lNOPE&4&l] &cYou lack the &a%perm% &cpermission.")
-									.replace("%perm%", c.getPermission()));
-					return true;
+									"&4&l[&c&lNOPE&4&l] &c你缺少&a%perm% &cpermission。")
+									.replace("%perm%", c.getPermission());
+					返回真；
 				}
-				MSG.tell(sender, "&4" + label + " > &cProper usage for " + c.getName());
-				MSG.tell(sender, "&7/" + label + " " + c.getName() + " " + c.getUsage());
-				MSG.tell(sender, result.getMessage());
-				return true;
+				MSG.tell(发件人, "&4" + 标签+ " + c.getName() 的&cProper使用量);
+				MSG.tell(发送者, "&7/" + 标签+ " + c.getName() + " + " + c.getUsage() );
+				MSG.tell(发送者，结果.getMessage())；
+				返回真；
 			}
 		}
-		return false;
+		返回 false;
 	}
 
-	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-		List<String> result = new ArrayList<>();
+	@覆盖
+	公开列表<String> onTabComplete(Commander Sender, Command Command, String label, String[args)
+		列表<String> 结果 = 新的数组列表<>();
 
-		for (Subcommand sub : cmds) {
-			List<String> aliases = sub.getAliases();
-			aliases.add(sub.getName());
-			List<String[]> completions = sub.tabCompletions(sender);
+		for (Subcommand sub-b: cmds)
+			邮件列表<String> 别名 = sub.getAliases();
+			别名.add(sub.getName() );
+			邮件列表<String[]> 补全 = sub.tabCompltions(发送者)；
 			if (args.length > 1) {
-				if (completions == null || completions.isEmpty())
-					continue;
-				if (completions.size() < args.length - 1)
-					continue;
-				if (!aliases.contains(args[0].toLowerCase()))
-					continue;
-				String[] res = completions.get(args.length - 2);
+				if (compltions == null || completitions.isEmpty())
+					继续；
+				如果(completitions.size) < args.length - 1)
+					继续；
+				如果(!aliases.contains(args[0].toLowerCase()))
+					继续；
+				字符串[] res = complettions.get(args.length - 2)；
 				if (res == null)
-					continue;
-				for (String r : res)
-					if (r.toLowerCase().startsWith(args[args.length - 1].toLowerCase()))
-						result.add(r);
-				continue;
+					继续；
+				适合(String r : )
+					如果(r.toLowerCase().startsWid(args[args.length - 1].toLowerCase()))
+						结果添加(r)；
+				继续；
 			}
-			for (String alias : aliases) {
-				if (alias.toLowerCase().startsWith(args[args.length - 1].toLowerCase()))
-					result.add(alias);
+			for (String alias : aliases)
+				如果(alias.toLowerCase().startsWid(args[args.length - 1].toLowerCase()))
+					结果添加(别名)；
 			}
 		}
 
-		return result.isEmpty() ? null : result;
+		返回 result.isEmpty() ? 空：结果;
 	}
 
 }
