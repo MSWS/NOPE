1c745a9d5bb50e52b06149228c321445bcd90e05
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:52:14 +1200

    New translations CommandResult.java (Portuguese, Brazilian)

--- t/Portuguese, Brazilian/src/xyz/msws/nope/commands/CommandResult.java
+++ t/Portuguese, Brazilian/src/xyz/msws/nope/commands/CommandResult.java
@@ -1,64 +1,64 @@
-package xyz.msws.nope.commands;
+pacote xyz.msws.nope.commands;
 
-import xyz.msws.nope.utils.MSG;
+importar xyz.msws.nope.utils.MSG;
 
 /**
- * Represents the result of a command.
+ * Representa o resultado de um comando.
  * 
- * @author imodm
+ * Iodm @autor
  *
  */
-public enum CommandResult {
+public enum CommandResultult {
 	/**
-	 * The command was completed successfully. A custom success message should be
-	 * sent.
+	 * O comando foi concluído com sucesso. Uma mensagem de sucesso personalizada deve ser
+	 * enviado.
 	 */
-	SUCCESS,
+	SUCESSO,
 	/**
-	 * The sender does not have the proper permissions for the command.
+	 * O remetente não tem as permissões adequadas para o comando.
 	 */
 	NO_PERMISSION,
 	/**
-	 * An argument is missing.
+	 * Está faltando um argumento.
 	 */
-	MISSING_ARGUMENT,
+	PLAYLIST_DESCRIPTION_DESCRIPTION
 	/**
-	 * An invalid argument is given.
+	 * Um argumento inválido é dado.
 	 */
 	INVALID_ARGUMENT,
 	/**
-	 * Only a player can use the command and the sender is not one.
+	 * Somente um jogador pode usar o comando e o remetente não é.
 	 */
-	PLAYER_ONLY,
+	PLAYER_APLIS,
 	/**
-	 * The executor did not give a player, same as
-	 * {@link CommandResult#MISSING_ARGUMENT} but more specific
+	 * O executor não deu um jogador, o mesmo que
+	 * {@link CommandResult#MISSING_ARGUMENT} mas mais específico
 	 */
-	PLAYER_REQUIRED,
+	PLAYER_REINTROD_TOOLTIP_TEXT
 	/**
-	 * An unknown error occured
+	 * Ocorreu um erro desconhecido
 	 */
-	ERROR;
+	ERRO;
 
 	public String getMessage() {
-		switch (this) {
-			case INVALID_ARGUMENT:
-				return MSG.getString("Command.InvalidArgument", "&cAn invalid argument was provided.");
-			case MISSING_ARGUMENT:
-				return MSG.getString("Command.MissingArgument", "&cYou are missing an argument.");
-			case NO_PERMISSION:
+		switch (isto) {
+			caso INVALID_ARGUMENT:
+				return MSG.getString("Command.InvalidArgument", "&cUm argumento inválido foi fornecido.");
+			caso MISSING_ARGUMENT:
+				return MSG.getString("Command.MissingArgument", "&cVocê está faltando um argumento.");
+			caso NO_PERMISSION:
 				return MSG.getString("Command.NoPermission",
-						"&4&l[&c&lNOPE&4&l] &cYou lack the &a%perm% &cpermission.");
+						"&4&l[&c&lNOPE&4&l] &cVocê não possui a &a permissão de%perm%.");
 			case PLAYER_ONLY:
 				return MSG.getString("Command.PlayerOnly",
-						"&cYou must specify a player to run this command as console.");
+						&cVocê precisa especificar um jogador para executar este comando como console.");
 			case PLAYER_REQUIRED:
-				return MSG.getString("Command.SpecifyPlayer", "&cYou must specify a player as an argument.");
+				return MSG.getString("Command.SpecifyPlayer", "&cVocê precisa especificar um jogador como um argumento.");
 			case SUCCESS:
-				return "";
-			default:
-				break;
+				retornar "";
+			padrão:
+				quebrar;
 		}
-		return "&4An error occured whilst executing the command.";
+		retorne "&4Um erro ocorreu durante a execução do comando";
 	}
 }
