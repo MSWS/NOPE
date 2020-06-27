a81e74e1157097b88a7a80cd0637c427996c6845
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:41:52 +1200

    New translations NOPECommand.java (Arabic)

--- t/Arabic/src/xyz/msws/nope/commands/NOPECommand.java
+++ t/Arabic/src/xyz/msws/nope/commands/NOPECommand.java
@@ -1,73 +1,73 @@
-package xyz.msws.nope.commands;
+حزمة xyz.msws.nope.command;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import xyz.msws.nope.NOPE;
-import xyz.msws.nope.commands.sub.BanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ChecksSubcommand;
-import xyz.msws.nope.commands.sub.ClearSubcommand;
-import xyz.msws.nope.commands.sub.EnablechecksSubcommand;
-import xyz.msws.nope.commands.sub.HelpSubcommand;
-import xyz.msws.nope.commands.sub.LookupSubcommand;
-import xyz.msws.nope.commands.sub.OnlineSubcommand;
-import xyz.msws.nope.commands.sub.ReloadSubcommand;
-import xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ReportSubcommand;
-import xyz.msws.nope.commands.sub.ResetSubcommand;
-import xyz.msws.nope.commands.sub.StatsSubcommand;
-import xyz.msws.nope.commands.sub.TestAnimationSubcommand;
-import xyz.msws.nope.commands.sub.TestlagSubcommand;
-import xyz.msws.nope.commands.sub.TimeSubcommand;
-import xyz.msws.nope.commands.sub.ToggleSubcommand;
-import xyz.msws.nope.commands.sub.TrustSubcommand;
-import xyz.msws.nope.commands.sub.VLSubcommand;
-import xyz.msws.nope.commands.sub.WarnSubcommand;
+استيراد xyz.msws.nope.NOPE؛
+استيراد xyz.msws.nope.commands.sub.BanwaveSubcommand؛
+استيراد xyz.msws.nope.commands.sub.ChecksSubcommand؛
+استيراد xyz.msws.nope.commands.sub.clearSubcommand؛
+استيراد xyz.msws.nope.commands.sub.EnablechecksSubcommand؛
+استيراد xyz.msws.nope.commands.sub.HelpSubcommand؛
+استيراد xyz.msws.nope.commands.sub.LookupSubcommand؛
+استيراد xyz.msws.nope.commands.sub.OnlineSubcommand؛
+استيراد xyz.msws.nope.commands.sub.ReloadSubcommand؛
+استيراد xyz.msws.nope.commands.sub.RemovebanwaveSubcommand؛
+استيراد xyz.msws.nope.commands.sub.ReportSubcommand؛
+استيراد xyz.msws.nope.commands.sub.ResetSubcommand؛
+استيراد xyz.msws.nope.commands.sub.StatsSubcommand؛
+استيراد xyz.msws.nope.commands.sub.TestAnimationSubcommand؛
+استيراد xyz.msws.nope.commands.sub.TestlagSubcommand؛
+استيراد xyz.msws.nope.commands.sub.TimeSubcommand؛
+استيراد xyz.msws.nope.commands.sub.ToggleSubcommand؛
+استيراد xyz.msws.nope.commands.sub.TrustSubcommand؛
+استيراد xyz.msws.nope.commands.sub.VLSubcommand؛
+استيراد xyz.msws.nope.commands.sub.WarnSubcommand؛
 
-public class NOPECommand extends AbstractCommand {
+الطبقة العامة NOPECommand توسع AbstractCommand {
 
-	private HelpSubcommand help;
+	مساعدة خاصة في الأمر المساعد؛
 
-	public NOPECommand(NOPE plugin) {
-		super(plugin);
-		help = new HelpSubcommand(plugin, this, 8);
+	الجمهور NOPEFAE (إضافة NOPE) {
+		فائق (إضافة)؛
+		المساعدة = مساعدة جديدة (الملحق، هذا 8)؛
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		label = label.toUpperCase();
-		if (super.onCommand(sender, command, label, args))
-			return true;
-		help.execute(sender, args);
-		return true;
+	@تجاوز
+	علبة عامة على القائد (قائد المرسل، أمر الأوامر ، تسمية String ، String[] args) {
+		تسمية = label.toUpperCase();
+		إذا (super.oncommand(sender, command, label, args))
+			إرجاع صحيح؛
+		help.execute(sender, arg);
+		إرجاع صحيح؛
 	}
 
-	@Override
-	public void enable() {
+	@تجاوز
+	الفراغ العام تمكين () {
 		super.enable();
-		cmds.add(new VLSubcommand(plugin));
-		cmds.add(new ClearSubcommand(plugin));
-		cmds.add(new ReportSubcommand(plugin));
-		cmds.add(new LookupSubcommand(plugin));
-		cmds.add(new ToggleSubcommand(plugin));
-		cmds.add(new StatsSubcommand(plugin));
-		cmds.add(new TimeSubcommand(plugin));
-		cmds.add(new BanwaveSubcommand(plugin));
-		cmds.add(new ReloadSubcommand(plugin));
-		cmds.add(new ResetSubcommand(plugin));
-		cmds.add(new TestlagSubcommand(plugin));
-		cmds.add(new RemovebanwaveSubcommand(plugin));
-		cmds.add(new EnablechecksSubcommand(plugin));
-		cmds.add(new OnlineSubcommand(plugin));
-		cmds.add(new TestAnimationSubcommand(plugin));
-		cmds.add(new WarnSubcommand(plugin));
-		cmds.add(new ChecksSubcommand(plugin));
-		cmds.add(new TrustSubcommand(plugin));
-		cmds.add(help);
+		cmds.add(VLSubcommand(plugin));
+		cmds.add(clearSubcommand(plugin))؛
+		cmds.add(جديد ReportSubcommand(plugin))؛
+		cmds.add(جديد LookupSubcommand(plugin))؛
+		cmds.add(جديد ToggleSubcommand(plugin))؛
+		cmds.add(New StatsSubcommand(plugin))؛
+		cmds.add(TimeSubcommand(plugin));
+		cmds.add(جديد BanwaveSubcommand(plugin))؛
+		cmds.add(جديد ReloadSubcommand(plugin))؛
+		cmds.add(ReetSubcommand(plugin));
+		cmds.add(جديد TestlagSubcommand(plugin))؛
+		cmds.add(new RemovebanwaveSubcommand(plugin))؛
+		cmds.add(جديد EnablechecksSubcommand(plugin))؛
+		cmds.add(OnlineSubcommand(plugin))؛
+		cmds.add(TestAnimationSubcommand(plugin))؛
+		cmds.add(WarnSubcommand(plugin))؛
+		cmds.add(ChecksSubcommand(plugin))؛
+		cmds.add(TrustSubcommand(plugin))؛
+		cmds.add(help)؛
 	}
 
-	@Override
-	public String getName() {
-		return "nope";
+	@تجاوز
+	المقطع العام getName() {
+		إرجاع "nope";
 	}
 }
