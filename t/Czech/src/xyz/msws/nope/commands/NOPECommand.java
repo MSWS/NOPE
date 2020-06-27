daeb867737dd69fd22bf8285e4c56ec505eb43c6
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:43:13 +1200

    New translations NOPECommand.java (Czech)

--- t/Czech/src/xyz/msws/nope/commands/NOPECommand.java
+++ t/Czech/src/xyz/msws/nope/commands/NOPECommand.java
@@ -1,73 +1,73 @@
-package xyz.msws.nope.commands;
+balíček xyz.msws.nope.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import xyz.msws.nope.NOPE;
-import xyz.msws.nope.commands.sub.BanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ChecksSubcommand;
-import xyz.msws.nope.commands.sub.ClearSubcommand;
-import xyz.msws.nope.commands.sub.EnablechecksSubcommand;
-import xyz.msws.nope.commands.sub.HelpSubcommand;
-import xyz.msws.nope.commands.sub.LookupSubcommand;
-import xyz.msws.nope.commands.sub.OnlineSubcommand;
-import xyz.msws.nope.commands.sub.ReloadSubcommand;
-import xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ReportSubcommand;
-import xyz.msws.nope.commands.sub.ResetSubcommand;
-import xyz.msws.nope.commands.sub.StatsSubcommand;
-import xyz.msws.nope.commands.sub.TestAnimationSubcommand;
-import xyz.msws.nope.commands.sub.TestlagSubcommand;
-import xyz.msws.nope.commands.sub.TimeSubcommand;
-import xyz.msws.nope.commands.sub.ToggleSubcommand;
-import xyz.msws.nope.commands.sub.TrustSubcommand;
-import xyz.msws.nope.commands.sub.VLSubcommand;
-import xyz.msws.nope.commands.sub.WarnSubcommand;
+importovat xyz.msws.nope.NOPE;
+importovat xyz.msws.nope.commands.sub.BanwaveSubcommand;
+importovat xyz.msws.nope.commands.sub.ChecksSubcommand;
+importovat xyz.msws.nope.commands.sub.ClearSubcommand;
+importovat xyz.msws.nope.commands.sub.EnablechecksSubcommand;
+importovat xyz.msws.nope.commands.sub.HelpSubcommand;
+importovat xyz.msws.nope.commands.sub.LookupSubcommand;
+importovat xyz.msws.nope.commands.sub.OnlineSubcommand;
+importovat xyz.msws.nope.commands.sub.ReloadSubcommand;
+importovat xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
+importovat xyz.msws.nope.commands.sub.ReportSubcommand;
+importovat xyz.msws.nope.commands.sub.ResetSubcommand;
+importovat xyz.msws.nope.commands.sub.StatsSubcommand;
+importovat xyz.msws.nope.commands.sub.TestAnimationSubcommand;
+importovat xyz.msws.nope.commands.sub.TestlagSubcommand;
+importovat xyz.msws.nope.commands.sub.TimeSubcommand;
+importovat xyz.msws.nope.commands.sub.ToggleSubcommand;
+importovat xyz.msws.nope.commands.sub.TrustSubcommand;
+importovat xyz.msws.nope.commands.sub.VLSubcommand;
+importovat xyz.msws.nope.commands.sub.WarnSubcommand;
 
-public class NOPECommand extends AbstractCommand {
+veřejná třída NOPECommand rozšiřuje AbstractCommand {
 
-	private HelpSubcommand help;
+	soukromá HelpSubcommand nápověda;
 
-	public NOPECommand(NOPE plugin) {
+	veřejný NOPECommand(NOPE plugin) {
 		super(plugin);
-		help = new HelpSubcommand(plugin, this, 8);
+		help = nový HelpSubcommand(plugin, this 8);
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		label = label.toUpperCase();
-		if (super.onCommand(sender, command, label, args))
-			return true;
-		help.execute(sender, args);
-		return true;
+	@override
+	veřejné boolean onCommand(CommandSender sender, velitelský příkaz, String label, String[] args) {
+		štítek = label.toUpperCase();
+		Pokud (super.onCommand(ender, příkaz, štítek, náklady))
+			zpáteční pravda;
+		help.execute(ender, arge);
+		zpáteční pravda;
 	}
 
-	@Override
+	@override
 	public void enable() {
 		super.enable();
-		cmds.add(new VLSubcommand(plugin));
-		cmds.add(new ClearSubcommand(plugin));
-		cmds.add(new ReportSubcommand(plugin));
+		cmds.add(nový VLSubcommand(plugin));
+		cmds.add(nový ClearSubcommand(plugin));
+		cmds.add(nová ReportSubcommand(plugin));
 		cmds.add(new LookupSubcommand(plugin));
 		cmds.add(new ToggleSubcommand(plugin));
-		cmds.add(new StatsSubcommand(plugin));
-		cmds.add(new TimeSubcommand(plugin));
-		cmds.add(new BanwaveSubcommand(plugin));
-		cmds.add(new ReloadSubcommand(plugin));
-		cmds.add(new ResetSubcommand(plugin));
-		cmds.add(new TestlagSubcommand(plugin));
-		cmds.add(new RemovebanwaveSubcommand(plugin));
-		cmds.add(new EnablechecksSubcommand(plugin));
-		cmds.add(new OnlineSubcommand(plugin));
-		cmds.add(new TestAnimationSubcommand(plugin));
-		cmds.add(new WarnSubcommand(plugin));
-		cmds.add(new ChecksSubcommand(plugin));
+		cmds.add(nový StatsSubcommand(plugin));
+		cmds.add(nový TimeSubcommand(plugin));
+		cmds.add(nový BanwaveSubcommand(plugin));
+		cmds.add(nové ReloadSubcommand(plugin));
+		cmds.add(nový ResetSubcommand(plugin));
+		cmds.add(nový TestlagSubcommand(plugin));
+		cmds.add(nový RemovebanwaveSubcommand(plugin));
+		cmds.add(nový EnablechecksSubcommand(plugin));
+		cmds.add(nový OnlineSubcommand(plugin));
+		cmds.add(nové TestAnimationSubcommand(plugin));
+		cmds.add(nový WarnSubcommand(plugin));
+		cmds.add(nový ChecksSubcommand(plugin));
 		cmds.add(new TrustSubcommand(plugin));
 		cmds.add(help);
 	}
 
-	@Override
+	@override
 	public String getName() {
-		return "nope";
+		vrátit „nope“;
 	}
 }
