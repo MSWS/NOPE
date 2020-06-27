70e864ce12a927d280c606069f87013171106757
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2020-06-27 15:51:40 +1200

    New translations NOPECommand.java (Chinese Simplified)

--- t/Chinese Simplified/src/xyz/msws/nope/commands/NOPECommand.java
+++ t/Chinese Simplified/src/xyz/msws/nope/commands/NOPECommand.java
@@ -1,73 +1,73 @@
-package xyz.msws.nope.commands;
+包 xyz.msws.nope.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import xyz.msws.nope.NOPE;
-import xyz.msws.nope.commands.sub.BanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ChecksSubcommand;
-import xyz.msws.nope.commands.sub.ClearSubcommand;
-import xyz.msws.nope.commands.sub.EnablechecksSubcommand;
-import xyz.msws.nope.commands.sub.HelpSubcommand;
-import xyz.msws.nope.commands.sub.LookupSubcommand;
-import xyz.msws.nope.commands.sub.OnlineSubcommand;
-import xyz.msws.nope.commands.sub.ReloadSubcommand;
-import xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
-import xyz.msws.nope.commands.sub.ReportSubcommand;
-import xyz.msws.nope.commands.sub.ResetSubcommand;
-import xyz.msws.nope.commands.sub.StatsSubcommand;
-import xyz.msws.nope.commands.sub.TestAnimationSubcommand;
-import xyz.msws.nope.commands.sub.TestlagSubcommand;
-import xyz.msws.nope.commands.sub.TimeSubcommand;
-import xyz.msws.nope.commands.sub.ToggleSubcommand;
-import xyz.msws.nope.commands.sub.TrustSubcommand;
-import xyz.msws.nope.commands.sub.VLSubcommand;
-import xyz.msws.nope.commands.sub.WarnSubcommand;
+导入 xyz.msws.nope.NOPE;
+导入 xyz.msws.nope.commands.sub.BanwaveSubcommand;
+导入 xyz.msws.nope.commands.sub.checksSubcommand;
+导入 xyz.msws.nope.commands.subClearSubcommand;
+导入 xyz.msws.nope.commands.sub.EnablechecksSubcommand;
+导入 xyz.msws.nope.commands.sub.HelpSubcommand;
+导入 xyz.msws.nope.commands.sub.LookupSubcommand;
+导入 xyz.msws.nope.commands.sub.OnlineSubcommand;
+导入 xyz.msws.nope.commands.sub.ReloadSubcommand;
+导入 xyz.msws.nope.commands.sub.RemovebanwaveSubcommand;
+导入 xyz.msws.nope.commands.sub.ReportSubcommand;
+导入 xyz.msws.nope.commands.sub.ResetSubcommand;
+导入 xyz.msws.nope.commands.sub.StatsSubcommand;
+导入 xyz.msws.nope.commands.sub.TestAnimationSubcommand;
+导入 xyz.msws.nope.commands.sub.TestlagSubcommand;
+导入 xyz.msws.nope.commands.sub.TimeSubcommand;
+导入 xyz.msws.nope.commands.sub.ToggleSubcommand;
+导入 xyz.msws.nope.commands.sub.TrustSubcommand;
+导入 xyz.msws.nope.commands.sub.VLSubcommand;
+导入 xyz.msws.nope.commands.sub.WarnSubcommand;
 
-public class NOPECommand extends AbstractCommand {
+公共课堂NOPECommand exts AbstractCommand
 
-	private HelpSubcommand help;
+	私有HelpSub命令帮助；
 
-	public NOPECommand(NOPE plugin) {
-		super(plugin);
-		help = new HelpSubcommand(plugin, this, 8);
+	公开NOPECommand(NOPE plugin)
+		super(插件)；
+		help = new HelpSubcommand(plugin, this 8);
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	@覆盖
+	Public boolian onCommander(Commander Sender, Command Command, String label, String[]args)
 		label = label.toUpperCase();
-		if (super.onCommand(sender, command, label, args))
-			return true;
-		help.execute(sender, args);
-		return true;
+		如果(super.onCommand(发送者、命令、标签、args))
+			返回真；
+		help.execute(发送者、参数)；
+		返回真；
 	}
 
-	@Override
-	public void enable() {
-		super.enable();
-		cmds.add(new VLSubcommand(plugin));
-		cmds.add(new ClearSubcommand(plugin));
-		cmds.add(new ReportSubcommand(plugin));
-		cmds.add(new LookupSubcommand(plugin));
-		cmds.add(new ToggleSubcommand(plugin));
-		cmds.add(new StatsSubcommand(plugin));
-		cmds.add(new TimeSubcommand(plugin));
-		cmds.add(new BanwaveSubcommand(plugin));
-		cmds.add(new ReloadSubcommand(plugin));
-		cmds.add(new ResetSubcommand(plugin));
-		cmds.add(new TestlagSubcommand(plugin));
-		cmds.add(new RemovebanwaveSubcommand(plugin));
-		cmds.add(new EnablechecksSubcommand(plugin));
-		cmds.add(new OnlineSubcommand(plugin));
-		cmds.add(new TestAnimationSubcommand(plugin));
-		cmds.add(new WarnSubcommand(plugin));
-		cmds.add(new ChecksSubcommand(plugin));
-		cmds.add(new TrustSubcommand(plugin));
-		cmds.add(help);
+	@覆盖
+	公开无效启用()
+		启用();
+		cmds.add(新 VLSubcommand(plugin));
+		cmds.add(新 ClearSubcommand(plugin));
+		cmds.add(新 ReportSubcommand(plugin));
+		cmds.add(新 LookupSubcommand(plugin));
+		cmds.add(新 ToggleSubcommand(plugin));
+		cmds.add(新 StatsSubcommand(plugin));
+		cmds.add(新 TimeSubcommand(plugin));
+		cmds.add(新 BanwaveSubcommand(plugin));
+		cmds.add(新 ReloadSubcommand(plugin));
+		cmds.add(新 ResetSubcommand(plugin));
+		cmds.add(新 TestlagSubcommand(plugin));
+		cmds.add(新 RemovebanwaveSubcommand(plugin));
+		cmds.add(新 EnablechecksSubcommand(plugin));
+		cmds.add(新 OnlineSubcommand(plugin));
+		cmds.add(新 TestAnimationSubcommand(plugin));
+		cmds.add(新 WarnSubcommand(plugin));
+		cmds.add(新 ChecksSubcommand(plugin));
+		cmds.add(新 TrustSubcommand(plugin));
+		cmds.add(帮助)；
 	}
 
-	@Override
-	public String getName() {
-		return "nope";
+	@覆盖
+	public String getName()
+		返回 "nope";
 	}
 }
